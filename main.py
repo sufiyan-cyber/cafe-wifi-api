e0ea330c56a59a4a6d4fd1a410f9ee2a24d7704e
sufiyan-cyber <sufiyankhan321123@gmail.com>
2026-03-27 22:51:23 +0530

    new changes postgres server and logging enabled and more

--- main.py
+++ main.py
@@ -2,30 +2,25 @@ from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
-
-'''
-Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
+from sqlalchemy import func
+import os
+import logging
+logging.basicConfig(level=logging.INFO)
+from dotenv import load_dotenv
+load_dotenv()
 app = Flask(__name__)
 
 # CREATE DB
 class Base(DeclarativeBase):
     pass
 # Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///cafes.db"
+)
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
-
+print("DB URI:", app.config['SQLALCHEMY_DATABASE_URI'])
 
 # Cafe TABLE Configuration
 class Cafe(db.Model):
@@ -59,10 +54,12 @@ import random
 
 @app.route("/random", methods=["GET"])
 def random1():
-    # Get all cafes
-    cafes = db.session.execute(db.select(Cafe)).scalars().all()
-    # Pick one at random
-    random_cafe = random.choice(cafes)
+    logging.info("Fetching random cafe")
+    stmt = db.select(Cafe).order_by(func.random()).limit(1)
+    random_cafe = db.session.execute(stmt).scalar()
+    if not random_cafe:
+        return jsonify(error="No cafes found in database"), 404
+
     # Return JSON
     return jsonify(
         id=random_cafe.id,
@@ -83,6 +80,7 @@ def random1():
 @app.route("/all", methods=["GET"])
 def get_all_cafes():
     # Use .execute + .select
+    logging.info("getting all cafes")
     result = db.session.execute(db.select(Cafe)).scalars().all()
     # Convert each row to dict
     cafes = [cafe.to_dict() for cafe in result]
@@ -91,6 +89,7 @@ def get_all_cafes():
 
 @app.route("/search/<loc>", methods=["GET"])
 def searching(loc):
+    logging.info("searching for cafe")
     stmt = db.select(Cafe).where(Cafe.location == loc)
     result = db.session.execute(stmt).scalars().all()
 
@@ -103,6 +102,10 @@ def searching(loc):
 
 @app.route("/add", methods=["POST"])
 def post_new_cafe():
+    if not request.form.get("name"):
+        logging.info("name is empty")
+        return jsonify(error="Name is required"), 400
+    logging.info("adding new cafe")
     new_cafe = Cafe(
         name=request.form.get("name"),
         map_url=request.form.get("map_url"),
@@ -126,28 +129,34 @@ def pricing(id):
 
     if cafe:
         # Get new price from query parameter (?new_price=...)
+        logging.info("getting newprice from query parameter")
         new_price = request.args.get("new_price")
 
         if new_price:
+            logging.info("updating to new price")
             cafe.coffee_price = new_price
             db.session.commit()
             return jsonify(success={"Updated": f"Price updated to {new_price}"})
         else:
+            logging.info("missing new_price info")
             return jsonify(error={"Missing": "Please provide a new_price query parameter."}), 400
     else:
+        logging.info("No cafe found")
         return jsonify(error={"Not Found": "Cafe with that ID does not exist."}), 404
 
 
 # HTTP DELETE - Delete Record
 
 
-API_KEY = "supersecretkey123"  # store securely in env variables in real apps
+API_KEY = os.getenv("API_KEY", "defaultkey")
 
 @app.route("/report-closed/<int:id>", methods=["DELETE"])
 def deleting(id):
     # Check API key from query parameter (?api_key=...)
+    logging.info("checking apikey in query parameter for deleting")
     client_key = request.args.get("api_key")
     if client_key != API_KEY:
+        logging.info("invalid key entered ")
         return jsonify(error="Invalid API key"), 403
 
     # Find the cafe by ID
@@ -155,8 +164,10 @@ def deleting(id):
     if cafe:
         db.session.delete(cafe)
         db.session.commit()
+        logging.info("cafe deleted")
         return jsonify(success="Cafe deleted"), 200
     else:
+        logging.info("cafe not found for deleting")
         return jsonify(error="Cafe not found"), 404
 
 
